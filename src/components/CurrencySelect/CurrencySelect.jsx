7076d66716657f0cefa11d8bd16169722e2016c8
johan@dospuntos.no <johan@dospuntos.no>
2021-03-03 01:40:26 -0400

    map through currencies

--- src/components/CurrencySelect/CurrencySelect.jsx
+++ src/components/CurrencySelect/CurrencySelect.jsx
@@ -1,37 +1,43 @@
+const currencies = [
+  "CAD",
+  "HKD",
+  "ISK",
+  "PHP",
+  "DKK",
+  "HUF",
+  "CZK",
+  "AUD",
+  "RON",
+  "SEK",
+  "IDR",
+  "INR",
+  "BRL",
+  "RUB",
+  "HRK",
+  "JPY",
+  "THB",
+  "CHF",
+  "SGD",
+  "PLN",
+  "BGN",
+  "TRY",
+  "CNY",
+  "NOK",
+  "NZD",
+  "ZAR",
+  "USD",
+  "MXN",
+  "ILS",
+  "GBP",
+  "KRW",
+  "MYR",
+];
+
 const CurrencySelect = ({ value, handleChange }) => (
   <select value={value} onChange={(e) => handleChange(e)}>
-    <option>CAD</option>
-    <option>HKD</option>
-    <option>ISK</option>
-    <option>PHP</option>
-    <option>DKK</option>
-    <option>HUF</option>
-    <option>CZK</option>
-    <option>AUD</option>
-    <option>RON</option>
-    <option>SEK</option>
-    <option>IDR</option>
-    <option>INR</option>
-    <option>BRL</option>
-    <option>RUB</option>
-    <option>HRK</option>
-    <option>JPY</option>
-    <option>THB</option>
-    <option>CHF</option>
-    <option>SGD</option>
-    <option>PLN</option>
-    <option>BGN</option>
-    <option>TRY</option>
-    <option>CNY</option>
-    <option>NOK</option>
-    <option>NZD</option>
-    <option>ZAR</option>
-    <option>USD</option>
-    <option>MXN</option>
-    <option>ILS</option>
-    <option>GBP</option>
-    <option>KRW</option>
-    <option>MYR</option>
+    {currencies.map((currency) => {
+      return <option>{currency}</option>;
+    })}
   </select>
 );
 
